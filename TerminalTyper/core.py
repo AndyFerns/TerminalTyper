e893731af396ccef5f7ae01187a675f9713004a3
AndyFerns <write2andrew.fernandes@gmail.com>
2025-10-23 04:46:20 +0530

    added wpm and accuracy calculator functions

--- TerminalTyper/core.py
+++ TerminalTyper/core.py
@@ -21,3 +21,37 @@ def generate_prompt(words_list: list[str], num_words: int = 20) -> str:
 
 # Load the words into a global constant so the file only has to be read once
 WORDS = load_words()
+
+def wpm_calculator(prompt_text: str, user_text: str, elapsed_time) -> float:
+    """
+    Standard calculation for Words Per Minute (WPM)
+    """
+    #Standardize "words" to 5 characters, including spaces
+    num_chars = len(user_text)
+    typed_words = num_chars / 5 
+    
+    # avoid zero division if elapsed time is 0 (edgecase)
+    if elapsed_time == 0:
+        return 0
+    
+    minutes = elapsed_time / 60
+    wpm = typed_words / minutes
+    return wpm
+    
+def acc_calculator(prompt_text: str, user_text: str) -> float:
+    """
+    Calculates typing accuracy
+    """
+    correct = 0
+    # iterate through the shorter string (between prompt and user)
+    for i in range(min((len(prompt_text), len(user_text)))):
+        if user_text[i] == prompt_text[i]:
+            correct += 1
+        
+    # Avoid division by 0 (edgecase)
+    if len(user_text) == 0:
+        return 0.00
+    
+    accuracy = (correct / len(user_text)) * 100
+    return accuracy
+
